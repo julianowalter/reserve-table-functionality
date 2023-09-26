5d7b9036003b4cda85ec01cd810b2bd826cebf0b
julianowalter <julianows92@gmail.com>
2023-09-26 14:18:09 -0300

    use semantic structures as components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Header from './components/Header'
+import Main from './components/Main'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
 }
 
