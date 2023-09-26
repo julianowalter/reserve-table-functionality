3303678c2662f1b02f93f9c8112880c22dc7915c
julianowalter <julianows92@gmail.com>
2023-09-26 14:13:49 -0300

    add semantic structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Logo from '../assets/Logo.svg'
+
+function Header() {
+    return (
+        <div>
+            <img src={Logo} alt='company logo'/>
+        </div>
+    )
+}
+
+export default Header
