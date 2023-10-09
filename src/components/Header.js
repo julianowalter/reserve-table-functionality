7d15326c019b40cfd3617fafc91d9f0fc29da286
julianowalter <julianows92@gmail.com>
2023-10-09 16:27:45 -0300

    change max-width in Header responsive animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import Logo from '../assets/Logo.svg'
 import './Header.css'
 import { useRef } from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 function Header() {
     const hamburgerRef = useRef(null);
     const navMenuRef = useRef(null);
 
     const toggleMenu = () => {
-    hamburgerRef.current.classList.toggle('active');
-    navMenuRef.current.classList.toggle('active');
-  };
+        hamburgerRef.current.classList.toggle('active');
+        navMenuRef.current.classList.toggle('active');
+    };
 
     return (
         <header>
             <nav className='navbar'>
-                <img src={Logo} alt='company logo' className='logo'/>
+                <img src={Logo} alt='company logo' className='logo' />
                 <ul className='nav-menu' ref={navMenuRef}>
                     <li className='nav-item'><a href='/' className='nav-link'><Link to="/">Home</Link></a></li>
                     <li className='nav-item'><a href='/menu' className='nav-link'><Link to="/menu">Menu</Link></a></li>
