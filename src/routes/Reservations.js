1cb34f49c9a85a515df75ae667e81c711fca832d
julianowalter <julianows92@gmail.com>
2023-10-17 15:35:16 -0300

    add table reservation functionality

--- src/routes/Reservations.js
+++ src/routes/Reservations.js
@@ -1,9 +1,39 @@
-function Reservations() {
-    return (
-      <div>
-        <h1>Reservations</h1>
-      </div>
-    );
-  }
+import { useReducer } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../components/Reservations.css';
+import { fetchAPI, submitAPI } from '../utils/fakeAPI';
+import BookingForm from '../components/BookingForm';
 
-  export default Reservations;
+const updateTimes = (availableTimes, date) => {
+  const response = fetchAPI(new Date(date));
+  return (response.length !== 0) ? response : availableTimes;
+};
+
+const initializeTimes = initialAvailableTimes =>
+  [...initialAvailableTimes, ...fetchAPI(new Date())];
+
+const Bookings = () => {
+  const [
+    availableTimes,
+    dispatchOnDateChange
+  ] = useReducer(updateTimes, [], initializeTimes);
+  const navigate = useNavigate();
+
+  const submitData = formData => {
+    const response = submitAPI(formData);
+    if (response) navigate('/');
+  };
+
+  return (
+    <div className="bookings">
+      <h2>Table reservation</h2>
+      <BookingForm
+        availableTimes={availableTimes}
+        dispatchOnDateChange={dispatchOnDateChange}
+        submitData={submitData}
+      />
+    </div>
+  );
+};
+
+export default Bookings;
